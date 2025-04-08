1266b01a9cb4543690af026a933a01ca0c1720e9
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-08 16:41:21 -0500

    save my file to main

--- App.java
+++ App.java
@@ -1,6 +1,9 @@
+package Mergebranch;
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Master branch");
+        System.out.println("Once again created");
     }
 }
